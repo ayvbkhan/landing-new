19fdfaf68a259d95f734e1050214a28bcdecb5b5
Ваше Имя <_email@example.com>
2024-09-30 17:13:08 +0500

    all pages

--- script.js
+++ script.js
@@ -26,3 +26,93 @@ sale();
 setInterval(() => { 
     sale();
 }, 1000); 
+
+
+let wrapper = document.querySelector(".wrapper")
+let popUp = document.querySelector(".wrapper .popUp")
+let typ = document.querySelector(".wrapper .thankYouPage")
+let btns = document.querySelectorAll("section button")
+let formBtn = document.querySelector(".form button")
+let username = document.getElementsByName("username")[0]
+let userphone = document.getElementsByName("userphone")[0]
+
+btns.forEach( btn => {
+    btn.addEventListener('click', showpopUp)
+})
+
+wrapper.addEventListener('click', hidepopUp)
+
+function showpopUp() {
+    wrapper.style.display = "flex"
+    popUp.style.display = "flex"
+    typ.style.display = "none"
+}
+
+function hidepopUp() {
+    let elem = event.target 
+
+    if(elem == wrapper) {
+        wrapper.style.display = "none"
+    }
+}
+
+function showTYP() {
+    popUp.style.display = "none"
+    top.style.display = "flex"
+}
+
+formBtn.addEventListener("click", sendForm)
+
+function sendForm() {
+    event.preventDefault()
+    if(username.value.length > 1 ) {
+        if(userphone.value.length == 13) {
+        showTYP()
+    } else {
+        console.log('Неверный номер')
+    }
+    } else {
+        console.log('Короткое имя')
+    }
+}
+
+function showTYP() {
+    popUp.style.display = "none"
+    typ.style.display = "flex"
+} 
+
+userphone.addEventListener('input', function(event) {
+    let input = event.target;
+    let value = input.value.replace(/\D/g, ''); // Удаляем все нецифровые символы
+
+    // Форматируем номер, если он начинается с 998
+    let formattedValue = '';
+
+    if (value.length > 0) {
+        formattedValue = '+998'; // Начинаем с кода страны
+
+        if (value.length > 2) {
+            formattedValue += '(' + value.substring(2, 4) + ')'; // Код региона
+        }
+
+        if (value.length > 4) {
+            formattedValue += value.substring(4, 7); // Первая часть номера
+        }
+
+        if (value.length > 7) {
+            formattedValue += '-' + value.substring(7, 9); // Вторая часть номера
+        }
+
+        if (value.length > 9) {
+            formattedValue += '-' + value.substring(9, 11); // Третья часть номера
+        }
+    }
+
+    input.value = formattedValue; // Устанавливаем новое значение в поле ввода
+
+    // Устанавливаем курсор в конец строки
+    setTimeout(() => {
+        const cursorPosition = formattedValue.length; // Устанавливаем курсор в конец
+        input.setSelectionRange(cursorPosition, cursorPosition);
+    }, 0);
+});
